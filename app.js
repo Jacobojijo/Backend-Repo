0ab4a7d4bdd7aa6b79f495b0f18f99fccc13fedb
Minstrel Nwachukwu <minstreln@gmail.com>
2024-11-16 21:57:17 +0100

    Setup updated

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const cors = require("cors"); // Import CORS middleware
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const AppError = require('./utils/appError');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const globalErrorHandler = require('./controllers/errorHandler/errorController');
+
+// ---------------- route modules go here -------------------------------------
+
+// ---------------------------------------------------------------------------
 
 const app = express();
-const PORT = process.env.PORT || 4001;
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("combined")); // Log all requests in the "combined" format
+app.use(cors())
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.set('trust proxy', 1);
+
+app.use(express.static(`${__dirname}/public}`));
+
+app.use(express.json());
+
+const limiter = rateLimit({
+  max: 10,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
+//-------------- Routes should go here ---------------------------
+
+//----------------------------------------------------------------
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server 🚨!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
