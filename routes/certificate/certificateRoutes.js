5c2fb1926d2ea97e999d2d6b9821b005bc7d9cd5
Jacobojijo <jacobojijo2017@gmail.com>
2024-11-26 16:40:12 +0300

    new changes

--- routes/certificate/certificateRoutes.js
+++ routes/certificate/certificateRoutes.js
@@ -4,10 +4,16 @@ const { protect } = require('../../controllers/student/studentAuthController');
 const {
     getCertificate,
     downloadCertificate,
+    getAllCertificates,
 } = require('../../controllers/certificate/certificateController');
 
+// Retrieve all certificates
+router.get('/', protect, getAllCertificates);
+
+// Retrieve a single certificate by its ID
 router.get('/:certificateID', protect, getCertificate);
 
+// Download a certificate as a PDF
 router.get('/download/:certificateID', protect, downloadCertificate);
 
 module.exports = router;
