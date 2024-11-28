c0fddeb051fc47d4d2092a15ff0deff680556816
Nellybii <nellychepkem2015@gmail.com>
2024-11-28 13:04:18 +0300

    unprotecting verification

--- routes/certificate/certificateRoutes.js
+++ routes/certificate/certificateRoutes.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../../controllers/student/studentAuthController');
 const {
     getCertificate,
     downloadCertificate,
     getAllCertificates,
 } = require('../../controllers/certificate/certificateController');
 
-// Retrieve all certificates
 router.get('/', protect, getAllCertificates);
 
-// Retrieve a single certificate by its ID
-router.get('/:certificateID', protect, getCertificate);
+router.get('/:certificateID', getCertificate);
 
 router.get('/download/:certificateID', downloadCertificate);
 
